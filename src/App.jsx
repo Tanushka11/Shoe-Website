a89863683697b6a9562b16910bd8419f37d98a61
Tanushka <tanushkalalwani09@gmail.com>
2024-07-08 18:01:59 +0530

    landing page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './components/Navigation'
+import HeroSection from './components/HeroSection'
+function App() {
+
+  return (
+    <div>
+      <Navigation/>
+      <HeroSection/>
+    </div>
+  )
+}
+
+export default App
